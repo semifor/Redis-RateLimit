973ecd0a9092f1512d4f87fbb8990677eba3131c
Marc Mims <marc@questright.com>
2017-08-22 23:30:47 -0700

    Numify rules values for JSON encoding
    Ensure the values for rules will be encoded to numeric values in JSON
    rather than strings.
    
    Also, ensure interval and limit are defined in rules (precision is
    optional.)

--- lib/Redis/RateLimit.pm
+++ lib/Redis/RateLimit.pm
@@ -66,12 +66,20 @@ has rules => (
 );
 
 around BUILDARGS => sub {
-    my ( $next, $self ) = splice @_, 0, 2;
+    my ( $next, $self ) = ( shift, shift );
 
     my $args = $self->$next(@_);
     my $rules = delete $args->{rules};
     $args->{rules} = [
-        map [ grep defined, @{$_}{qw/interval limit precision/} ], @$rules
+        map {
+            my $rule = $_;
+            defined $rule->{$_} || croak "$_ undefined" for qw/interval limit/;
+            [
+                map 0+$_, # numify for later JSON encoding
+                @{$rule}{qw/interval limit/},
+                grep defined, $rule->{precision}
+            ];
+        } @$rules
     ];
 
     return $args;
